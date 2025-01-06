f8add8e51d418a4b252ee27078d2be4ba63bf4be
Dima Chukhlancev <htmlweb78@gmail.com>
2025-01-06 09:40:46 +0300

    Started implementing JCross class

--- src/main/kotlin/JCross.kt
+++ src/main/kotlin/JCross.kt
@@ -1,4 +1,5 @@
 package org.example
 
-class JCross {
+class JCross(val rowHints: List<List<Int>>, val colHints: List<List<Int>>) {
+    val grid: List<List<Int>> = List(colHints.size) { List(rowHints.size) { 0 } }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 package org.example
 
 fun main() {
+    val cross = JCross(listOf(listOf(1, 2), listOf(1, 3)), listOf(listOf(1, 2)))
     println("Hello World!")
 }
